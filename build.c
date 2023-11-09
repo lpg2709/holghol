c254a0689e17c4bb68689d315fb8c56f8f53b825
Leonardo Padilha Guarezi <leonardo_guarezi@hotmail.com>
2023-11-08 21:00:00 -0300

    Build ajust

--- build.c
+++ build.c
@@ -3,27 +3,34 @@
 #include "./src/holghol.h"
 
 int main(int argc, char **argv) {
+	const char *output = "../holghol.h";
+	//const char *output = NULL; // Print to stdou
+
 	const char *intro[] = {
-		"src/HEADER",
+        "../src/HEADER",
 	};
 	const char *includes_rm[] = {
 		"holghol.h",
 	};
 	const char *api[] = {
-		"src/holghol.h",
-		"src/ghol_core.c",
-		"src/ghol_files.c",
-		"src/ghol_utils.c",
+		"../src/holghol.h",
+        "../src/ghol_core.c",
+        "../src/ghol_files.c",
+        "../src/ghol_utils.c",
 	};
-	// const char *footer[] = {
-	// 	"LICENCE",
-	// 	"EXEMPLES",
-	// };
+	const char *footer[] = {
+		"../src/CHANGELOG",
+		"../src/CREDITS",
+		"../src/LICENSE",
+	};
 
-	gholCredits();
-	gholPlainFile(intro, GHOL_ARRAY_SIZE(intro));
-	gholLibFile(api, GHOL_ARRAY_SIZE(api), includes_rm, GHOL_ARRAY_SIZE(includes_rm));
-	//_gholRemoveIncludes("#include \"holghol.h\"\n#include \"teste.h\"\n#include \"mais.h\"\nint main() {\n  return 0; \n}", api_header, GHOL_ARRAY_SIZE(api_header));
+	gholCredits(output);
+	gholPlainFile(intro, GHOL_ARRAY_SIZE(intro), output);
+	gholIfdefine("HOLGHOL", output);
+	gholLibFile(api, GHOL_ARRAY_SIZE(api), includes_rm, GHOL_ARRAY_SIZE(includes_rm), output);
+	gholEndif("HOLGHOL", output);
+	gholPlainFile(footer, GHOL_ARRAY_SIZE(footer), output);
+
 	return 0;
 }
 
